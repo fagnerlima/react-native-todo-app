94862ef5f568f4df42c2f85ce9a7d116956d3cc8
Fagner <contato@fagnerlima.pro.br>
2020-07-06 13:04:59 -0300

    Add FloatButton component

---
+++ components/FloatButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+export interface FloatButtonProps {
+  iconName: string;
+}
+
+export default (props: FloatButtonProps) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Icon name={props.iconName} size={30} color="#01a699" />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderRadius: 100,
+    borderWidth: 1,
+    bottom: 10,
+    height: 70,
+    justifyContent: 'center',
+    position: 'absolute',
+    right: 10,
+    width: 70,
+  }
+});
